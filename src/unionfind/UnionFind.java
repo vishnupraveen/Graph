5f26101df3904a52bc888aa399a40e6f31797c63
Vishnu_Praveen <vishnupraveenblr@gmail.com>
2020-06-01 16:49:28 +0530

    Union Find Class

---
+++ src/unionfind/UnionFind.java
@@ -0,0 +1,65 @@
+package unionfind;
+
+public class UnionFind {
+    int size;
+    int[] id;
+    int[] sz;
+    int numComponents;
+
+    public UnionFind(int size) {
+        if(size<=0)
+            throw new IllegalArgumentException("Invalid Size");
+        this.size = size;
+        id=new int[size];
+        sz=new int[size];
+
+        for (int i = 0; i < size; i++) {
+            id[i]=i;
+            sz[i]=1;
+        }
+    }
+
+    public int find(int node)
+    {
+        int root=node;
+        while(root!=id[root]) root=id[root];
+        //compress path
+        while(node!=root)
+        {
+            int next=id[node];
+            id[node]=root;
+            node=next;
+        }
+    return root;
+    }
+
+    public boolean isConnected(int p, int q)
+    {
+        return  find(p)==find(q);
+    }
+    public int componentSize(int node)
+    {
+        return sz[node];
+    }
+
+    public void unify(int p , int q)
+    {
+        if(isConnected(p,q)) return;
+
+        int root1=find(p);
+        int root2=find(q);
+
+        if(sz[root1]<sz[root2])
+        {
+            sz[root2]+=sz[root1];
+            id[root1]=root2;
+        }
+        else
+        {
+            sz[root1]+=sz[root2];
+            id[root2]=root1;
+        }
+
+    }
+
+}
